f24e0768c72d09317f199f6154ffdc8bf81bc8c4
Izabella Pavlova <izabella1612@mail.ru>
2023-02-03 01:27:49 +0300

    review-1

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,9 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let submitButton = document.querySelector('.form__submit-button');
 
+let userCard = getUserCard();
+let newUserCard = getProfileForm()
+
 function getUserCard() {
   let profileName = document.querySelector('.profile__name');
   let profileDescription = document.querySelector('.profile__description');
@@ -10,14 +13,12 @@ function getUserCard() {
 }
 
 function getProfileForm() {
-  let newProfileName = document.querySelector('.form__input-text-profile-name');
-  let newProfileDescription = document.querySelector('.form__input-text-profile-description');
+  let newProfileName = document.querySelector('.form__input_text-profile_name');
+  let newProfileDescription = document.querySelector('.form__input_text-profile-description');
   return {"newProfileName": newProfileName, "newProfileDescription": newProfileDescription}
 }
 
 function openPopup() {
-  let userCard = getUserCard();
-  let newUserCard = getProfileForm()
   newUserCard.newProfileName.value = userCard.profileName.textContent;
   newUserCard.newProfileDescription.value = userCard.profileDescription.textContent;
   popup.classList.add('popup_opened');
@@ -27,9 +28,8 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-function updateUserCard() {
-  let userCard = getUserCard();
-  let newUserCard = getProfileForm()
+function updateUserCard(evt) {
+  evt.preventDefault();
   userCard.profileName.textContent = newUserCard.newProfileName.value;
   userCard.profileDescription.textContent = newUserCard.newProfileDescription.value;
   closePopup();
@@ -37,4 +37,4 @@ function updateUserCard() {
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-submitButton.addEventListener('click', updateUserCard);
+submitButton.addEventListener('submit', updateUserCard);
