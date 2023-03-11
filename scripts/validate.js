9709021cf4bd97138899f8f596dbfe08213e598b
Izabella Pavlova <izabella1612@mail.ru>
2023-03-11 21:59:32 +0300

    add validations

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,1 +1,66 @@
-const selectors = {};
+const selectors = {
+  formSelector: '.form',
+  InputSelector: '.form__input',
+  submitButtonSelector: '.form__submit-button',
+  inactiveButtonClass: 'form__submit-button_disabled',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__input-error_visible'
+};
+
+const isValid = (formElement, inputElement, selectors) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, selectors);
+  } else {
+    hideInputError(formElement, inputElement, selectors);
+  }
+};
+
+const showInputError = (formElement, inputElement, errorMessage, selectors) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.add(selectors.errorClass);
+  errorElement.textContent = errorMessage;
+  inputElement.classList.add(selectors.inputErrorClass);
+};
+
+const hideInputError = (formElement, inputElement, selectors) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.remove(selectors.errorClass);
+  errorElement.textContent = '';
+  inputElement.classList.remove(selectors.inputErrorClass);
+};
+
+// add listeners for every input in a form
+const setEventListeners = (formElement, selectors) => {
+  const inputList = Array.from(formElement.querySelectorAll(selectors.InputSelector));
+  const buttonElement = formElement.querySelector(selectors.submitButtonSelector);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement, selectors);
+      toggleButtonState(inputList, buttonElement, selectors);
+    });
+  });
+};
+
+// add listeners to every form
+const enableValidation = (selectors) => {
+  const formList = Array.from(document.querySelectorAll(selectors.formSelector));
+  formList.forEach((formElement) => {
+    setEventListeners(formElement, selectors);
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+const toggleButtonState = (inputList, buttonElement, selectors) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(selectors.inactiveButtonClass);
+  } else {
+    buttonElement.classList.remove(selectors.inactiveButtonClass);
+  }
+};
+
+enableValidation(selectors);
